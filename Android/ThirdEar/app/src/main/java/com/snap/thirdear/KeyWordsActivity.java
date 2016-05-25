5eefdafa0057b39f386c22933822a0064c833a41
hrajal <hrajalekshmi@sapient.com>
2016-05-25 11:03:45 -0400

    Show keyword from DB

--- Android/ThirdEar/app/src/main/java/com/snap/thirdear/KeyWordsActivity.java
+++ Android/ThirdEar/app/src/main/java/com/snap/thirdear/KeyWordsActivity.java
@@ -1,13 +1,67 @@
 package com.snap.thirdear;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+import com.snap.thirdear.adapter.ExpandableListAdapter;
+import com.snap.thirdear.db.DataBaseHelper;
+import com.snap.thirdear.db.Groups;
+import com.snap.thirdear.db.Trigger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class KeyWordsActivity extends AppCompatActivity {
+
+
+    private DataBaseHelper dataBaseHelper;
+    private ExpandableListView expListView;
+    private ExpandableListAdapter listAdapter;;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key_words);
+        expListView = (ExpandableListView) findViewById(R.id.expandableListView);
+
+        dataBaseHelper = new DataBaseHelper(this);
+        ArrayList<Groups> groupsList = dataBaseHelper.getAllGroups();
+        if (groupsList.isEmpty()) {
+            //show no keyword available Dialog
+            keywordUnavailableDialog();
+        } else {
+            List<String> groupNameList = new ArrayList<>();
+            HashMap<String, List<String>> triggersForEachGroup = new HashMap<>();
+            for (Groups group : groupsList) {
+                groupNameList.add(group.getName());
+                ArrayList<Trigger> triggerList = dataBaseHelper.getAllTriggersForGroup(group.get_id());
+                List<String> triggerTextList = new ArrayList<>();
+                for (Trigger trigger : triggerList) {
+                    triggerTextList.add(trigger.getTriggerText());
+                }
+                triggersForEachGroup.put(group.getName(), triggerTextList);
+            }
+            listAdapter = new ExpandableListAdapter(this, groupNameList, triggersForEachGroup);
+            expListView.setAdapter(listAdapter);
+        }
     }
+
+    private void keywordUnavailableDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.title_key_words)
+                .setMessage(R.string.keywords_unavailable);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
