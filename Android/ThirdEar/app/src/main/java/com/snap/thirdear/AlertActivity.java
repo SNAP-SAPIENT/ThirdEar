bd2b214bb6ad8c2da8f7431f1d0f8cd323d80efb
hrajal <hrajalekshmi@sapient.com>
2016-05-31 20:03:56 -0400

    Adding sound level alert preference

--- Android/ThirdEar/app/src/main/java/com/snap/thirdear/AlertActivity.java
+++ Android/ThirdEar/app/src/main/java/com/snap/thirdear/AlertActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.snap.thirdear.service.BackgroundSpeechRecognizer;
+import com.snap.thirdear.service.SoundLevelDetector;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -22,6 +23,7 @@ public class AlertActivity extends AppCompatActivity {
     private TextView alertText;
     private ImageView alertImg;
     private String TAG = AlertActivity.class.getName();
+    private String alertFor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +32,36 @@ public class AlertActivity extends AppCompatActivity {
         alertText = (TextView) findViewById(R.id.alertMsg);
         alertImg = (ImageView) findViewById(R.id.alert_image);
         Intent intent = getIntent();
-        String group = intent.getStringExtra(getString(R.string.intent_group));
-        String trigger = intent.getStringExtra(getString(R.string.intent_trigger));
-        String imageName = intent.getStringExtra(getString(R.string.intent_img));
-        String voiceProfile = intent.getStringExtra(getString(R.string.voice_profile));
-        alertText.setText(trigger);
-        int id = getResources().getIdentifier(imageName, "drawable", this.getPackageName());
-        Log.d(TAG, "onCreate: img id" + id);
-        alertImg.setImageResource(id);
-        Log.d(TAG, "onCreate: Voice profile: " + voiceProfile);
-        if(!voiceProfile.equalsIgnoreCase(getString(R.string.pref_selecProfile_default)))
-            playSound(voiceProfile, group);
+
+        alertFor= intent.getStringExtra(getString(R.string.alert_for_keywords));
+        if(alertFor.equals(getString(R.string.alert_for_keywords))) {
+            String group = intent.getStringExtra(getString(R.string.intent_group));
+            String trigger = intent.getStringExtra(getString(R.string.intent_trigger));
+            String imageName = intent.getStringExtra(getString(R.string.intent_img));
+            String voiceProfile = intent.getStringExtra(getString(R.string.voice_profile));
+            alertText.setText(trigger);
+            int id = getResources().getIdentifier(imageName, "drawable", this.getPackageName());
+            Log.d(TAG, "onCreate: img id" + id);
+            alertImg.setImageResource(id);
+            Log.d(TAG, "onCreate: Voice profile: " + voiceProfile);
+            if (!voiceProfile.equalsIgnoreCase(getString(R.string.pref_selecProfile_default)))
+                playSound(voiceProfile, group);
+        }else if (alertFor.equals(getString(R.string.alert_for_noiseLevel))){
+            String imageName = intent.getStringExtra(getString(R.string.intent_img));
+            String amp = intent.getStringExtra(getString(R.string.intent_amp));
+            alertText.setText("Noise detected...");
+            alertText.setText(amp);
+            int id = getResources().getIdentifier(imageName, "drawable", this.getPackageName());
+            Log.d(TAG, "onCreate: img id" + id);
+            alertImg.setImageResource(id);
+            playSound(R.raw.hareesh_disaster);
+        }
         hide();
+    }
+
+    private void playSound(int soundId) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, soundId);
+        mediaPlayer.start();
     }
 
     private void playSound(String voiceProfile, String group) {
@@ -65,8 +85,13 @@ public class AlertActivity extends AppCompatActivity {
     }
 
     public void closeAlert(View view){
-        stopService(new Intent(getBaseContext(), BackgroundSpeechRecognizer.class));
-        startService(new Intent(getBaseContext(), BackgroundSpeechRecognizer.class));
+        if(alertFor.equals(getString(R.string.alert_for_keywords))) {
+            stopService(new Intent(getBaseContext(), BackgroundSpeechRecognizer.class));
+            startService(new Intent(getBaseContext(), BackgroundSpeechRecognizer.class));
+        }else{
+            stopService(new Intent(getBaseContext(), SoundLevelDetector.class));
+            startService(new Intent(getBaseContext(), SoundLevelDetector.class));
+        }
         finish();
     }
 
--- Android/ThirdEar/app/src/main/java/com/snap/thirdear/service/SoundLevelDetector.java
+++ Android/ThirdEar/app/src/main/java/com/snap/thirdear/service/SoundLevelDetector.java
@@ -3,12 +3,17 @@ package com.snap.thirdear.service;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaRecorder;
-import android.os.Handler;
+import android.os.Environment;
+    import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.snap.thirdear.AlertActivity;
+import com.snap.thirdear.R;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,6 +25,7 @@ public class SoundLevelDetector extends Service {
     private static final String TAG = SoundLevelDetector.class.getName();
     private MediaRecorder mRecorder;
     Thread recordSound;
+    File audiofile = null;
 
     @Nullable
     @Override
@@ -63,28 +69,65 @@ public class SoundLevelDetector extends Service {
             mRecorder.release();
             mRecorder = null;
         }
+        destroyThread();
+    }
+
+    private void destroyThread() {
+//        recordSound.notify();
         recordSound.interrupt();
     }
 
 
     private void getSound(){
-            mRecorder = new MediaRecorder();
-            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            mRecorder.setOutputFile("/dev/null");
-            try {
-                mRecorder.prepare();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        File dir = Environment.getExternalStorageDirectory();
+        try {
+            audiofile = File.createTempFile("sound", ".3gp", dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mRecorder.setOutputFile(audiofile.getAbsolutePath());
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mRecorder.start();
     }
 
 
     private void checkNoiseLevel(){
         double amp = getAmplitude();
+        showAlertScreen(amp);
         Log.d(TAG, "checkNoiseLevel: amp: " +  amp);
+    }
+
+    private void showAlertScreen(double amp) {
+        Double limit = new Double(15);
+        Double ampDouble = new Double(amp);
+        if(ampDouble.isInfinite() || ampDouble.isNaN()){
+            Log.d(TAG, "showAlertScreen: not valid values");
+        }else if(ampDouble.compareTo(limit) >= 0 ){
+            destroyThread();
+            Intent intent = new Intent(getApplicationContext(), AlertActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(getString(R.string.intent_group), "NoiseLevel");
+            intent.putExtra(getString(R.string.intent_img), "alert_emergency");
+            String ampString = Double.toString(amp);
+            Log.d(TAG, "showAlertScreen: ampString: " + ampString);
+            if (ampString.contains(".")) {
+                String ampSplit = ampString.substring(0,ampString.indexOf("."));
+                Log.d(TAG, "showAlertScreen: ampSplit: " + ampSplit);
+                intent.putExtra(getString(R.string.intent_amp), "Sound level : " + ampSplit);
+            }else{
+                intent.putExtra(getString(R.string.intent_amp), ampString);
+            }
+            intent.putExtra(getString(R.string.alert_for_keywords), getString(R.string.alert_for_noiseLevel));
+            startActivity(intent);
+        }
     }
 
     public double getAmplitude() {
